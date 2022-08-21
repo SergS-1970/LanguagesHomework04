64860947622a4ba3316a1f85e9f9e09c0cd8817f
Serg_Sytnikov <tlingaal1996@gmail.com>
2022-08-21 06:36:49 +0300

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+
+Console.WriteLine("Введите значение элементов массива: "); 
+for (int i = 0; i < size; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"[{String.Join(",", array)}]");
