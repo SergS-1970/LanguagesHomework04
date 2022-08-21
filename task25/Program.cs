aa027c67e2b5b42ee83e7ed7dc7483037a824fc9
Serg_Sytnikov <tlingaal1996@gmail.com>
2022-08-21 04:01:28 +0300

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+ и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16 */
+
+Console.Write("Введите основание степени A: ");
+double numberA = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите натуральный показатель степени B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+double AToDegreeB = 1;
+for (int exponent = 1; exponent <= numberB; exponent++)
+{
+    AToDegreeB = AToDegreeB * numberA;
+}
+Console.WriteLine($"{numberA} в степени {numberB} = {AToDegreeB}");
