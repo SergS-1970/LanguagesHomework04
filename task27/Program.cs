524f9476049d2704a3bb8985b3c2794007f35354
Serg_Sytnikov <tlingaal1996@gmail.com>
2022-08-21 06:04:33 +0300

    task27 для целых чисел

--- task27/Program.cs
+++ task27/Program.cs
@@ -7,11 +7,12 @@ Console.Write("Введите число N: ");
 int numberN = Convert.ToInt32(Console.ReadLine());
 
 
+if (numberN < 0) numberN *= -1;
 int SumDigitN = 0;
-while (numberN / 1 > 0)
+while (numberN / 1 != 0)
 {
-    SumDigitN = SumDigitN + (numberN % 10);
-    numberN = numberN / 10;
+    SumDigitN += (numberN % 10);
+    numberN /= 10;
 }
 
 Console.WriteLine($"Сумма цифр в числе = {SumDigitN}");
